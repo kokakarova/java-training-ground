3a912da304fd344f95c63cbc1b1fe9776c23b96b
kokakarova <152164001+kokakarova@users.noreply.github.com>
2023-12-18 19:22:54 +0100

    adding txt file with source link for the exercise

--- backspaceStringCompare/src/test/java/SolutionTest.java
+++ backspaceStringCompare/src/test/java/SolutionTest.java
@@ -9,13 +9,20 @@ class SolutionTest {
     @DisplayName("True for ab#c equals ad#c")
     void shouldReturnTrueForAbHashcAndAdHasC() {
         Solution solution = new Solution();
-        assertTrue(solution.backspaceCompare("ab#c#", "ad#c"));
+        assertTrue(solution.backspaceCompare("ab#c", "ad#c"));
     }
     @Test
     @DisplayName("True for ab## equals c#d#")
     void shouldReturnTrueAbHashHashAndCHashDHash() {
         Solution solution = new Solution();
         assertTrue(solution.backspaceCompare("ab##", "c#d#"));
+    }
+
+    @Test
+    @DisplayName("False for a#c equals b")
+    void shouldReturnFalseForAHashCAndB() {
+        Solution solution = new Solution();
+        assertFalse(solution.backspaceCompare("a#c", "b"));
     }
 
 }
