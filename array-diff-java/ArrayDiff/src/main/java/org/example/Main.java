b6f7be04e327a1cefce7c088febaa7e7ca40104e
kokakarova <kostadinkakarova@gmail.com>
2024-01-28 23:14:08 +0100

    add unfinished code for arrayDif

---
+++ array-diff-java/ArrayDiff/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        public static int[] arrayDiff(int[] arr1, int[] arr2) {
+//            int[] arr1 = {1, 2};
+//            int[] arr2 = {1};
+            boolean evaluator = false;  // arr1_element != arr2_element
+
+            for (int i : arr1) {
+                while (!evaluator) {
+                    if ()
+                }
+            }
+            return new int[] = {1};
+        }
+    }
+}
