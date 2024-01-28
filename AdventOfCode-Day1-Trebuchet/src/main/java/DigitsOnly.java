20811bc9e8143d2548622c9507679462e12f42dc
kokakarova <kostadinkakarova@gmail.com>
2024-01-29 00:25:42 +0100

    feat: partially implement  reading for numbers in letters

---
+++ AdventOfCode-Day1-Trebuchet/src/main/java/DigitsOnly.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class DigitsOnly {
+    private static final String fileName = "readFile.txt";
+    private static int result = 0;
+
+    public static int getResultDigitsOnly() {
+        try (InputStream file = Trebuchet.class.getClassLoader().getResourceAsStream(fileName)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(file));
+
+
+            for (String s = reader.readLine(); s != null; s = reader.readLine()) {
+                String digitString = s.replaceAll("[^0-9]", "");
+
+                int firstDigit = 0;
+                int secondDigit = 0;
+                if (digitString.length() == 1) {
+                    firstDigit = Integer.parseInt(digitString);
+                    secondDigit = Integer.parseInt(digitString);
+                    result += firstDigit * 10 + secondDigit;
+                } else {
+                    firstDigit = Character.getNumericValue(digitString.charAt(0));
+                    secondDigit = Character.getNumericValue(digitString.charAt(digitString.length() - 1));
+                    result += firstDigit * 10 + secondDigit;
+                }
+            }
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}
--- AdventOfCode-Day1-Trebuchet/src/main/java/Trebuchet.java
+++ AdventOfCode-Day1-Trebuchet/src/main/java/Trebuchet.java
@@ -2,32 +2,6 @@ import java.io.*;
 
 public class Trebuchet {
     public static void main(String[] args) {
-
-        String fileName = "readFile.txt";
-        int result = 0;
-
-        try (InputStream file = Trebuchet.class.getClassLoader().getResourceAsStream(fileName)) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(file));
-
-            int firstDigit = 0;
-            int secondDigit = 0;
-
-            for (String s = reader.readLine(); s != null; s = reader.readLine()) {
-                String digitString = s.replaceAll("[^0-9]", "");
-
-                if (digitString.length() == 1) {
-                    firstDigit = Integer.parseInt(digitString);
-                    secondDigit = Integer.parseInt(digitString);
-                    result += firstDigit * 10 + secondDigit;
-                } else {
-                    firstDigit = Character.getNumericValue(digitString.charAt(0));
-                    secondDigit = Character.getNumericValue(digitString.charAt(digitString.length() - 1));
-                    result += firstDigit * 10 + secondDigit;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(result);
+        System.out.println(DigitsOnly.getResultDigitsOnly());
     }
 }
