16eb254c229478b86ab94fb2c7e995716a677a7b
kokakarova <kostadinkakarova@gmail.com>
2024-01-29 21:27:56 +0100

    create readPuzzle method

--- AdventOfCode-Day2-CubeConundrum/src/main/java/CubeConundrum.java
+++ AdventOfCode-Day2-CubeConundrum/src/main/java/CubeConundrum.java
@@ -1,8 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 /**
  * which games would have been possible if the bag contained only
- *      12 red cubes,
- *      13 green cubes, &
- *      14 blue cubes?
+ * 12 red cubes,
+ * 13 green cubes, &
+ * 14 blue cubes?
+ * <p>
+ * Game 1:  2 blue, 4 green;
+ * 7 blue, 1 red, 14 green;
+ * 5 blue, 13 green, 1 red;
+ * 1 red, 7 blue, 11 green
  */
 public class CubeConundrum {
 
@@ -11,6 +26,47 @@ public class CubeConundrum {
     static int maxRedCubes = 12;
     static int maxGreenCubes = 13;
     static int maxBlueCubes = 14;
+
+    static final String fileName = "puzzleInput.txt";
+
+    public static void readPuzzle(String fileName) {
+
+        InputStream file = CubeConundrum.class.getClassLoader().getResourceAsStream(fileName);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file));
+        int result = 0;
+
+        final Predicate<String> checkIfIsPossible = (str) -> {
+            // if > 12 for Red -> false
+            // if > 13 for Green -> false
+            // if > 14 for Blue -> false
+            var arrSplitString = str.split(" ");
+            arrSplitString.
+            if ((arrSplitString[1].contains("Red")
+                    && Integer.parseInt(arrSplitString[0]) > 12)
+                    || (arrSplitString[1].contains("Green")
+                    && Integer.parseInt(arrSplitString[0]) > 13)
+                    || (arrSplitString[1].contains("Blue")
+                    && Integer.parseInt(arrSplitString[0]) > 14)) {
+
+                return false;
+            }
+            return true;
+        };
+
+        try {
+            for (String s = reader.readLine(); s != null; s = reader.readLine()) {
+                var arrString1 = s.split("[:,;]");
+                boolean isPossible = Stream.of(arrString1)
+                        .filter(checkIfIsPossible).isParallel();
+                if (isPossible) {
+                    result += Integer.parseInt(arrString1[0].split(" ")[1]);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 
 }
---
+++ AdventOfCode-Day2-CubeConundrum/src/test/java/CubeConundrumTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+
+class CubeConundrumTest {
+
+    @Test
+    void firstTest() {
+        // Arrange
+        String fileName = "puzzleInput.txt";
+        // Act
+        CubeConundrum.readPuzzle(fileName);
+        // Assert
+    }
+}
