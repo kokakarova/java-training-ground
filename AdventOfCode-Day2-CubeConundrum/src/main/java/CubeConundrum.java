f78cf84dd2a7179da01e01409afd4b7aca6a553a
kokakarova <kostadinkakarova@gmail.com>
2024-01-29 18:55:42 +0100

    initial commit

---
+++ AdventOfCode-Day2-CubeConundrum/src/main/java/CubeConundrum.java
@@ -0,0 +1,13 @@
+/**
+ * which games would have been possible if the bag contained only
+ *      12 red cubes,
+ *      13 green cubes, &
+ *      14 blue cubes?
+ */
+public class CubeConundrum {
+    static int maxRedCubes = 12;
+    static int maxGreenCubes = 13;
+    static int maxBlueCubes = 14;
+
+
+}
