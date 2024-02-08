8a7e916bd106c02c4e3ec7d5b274d3253c84e493
kokakarova <kostadinkakarova@gmail.com>
2024-02-08 23:35:34 +0100

    implement solution to codewars kata array-diff-java

--- array-diff-java/ArrayDiff/src/main/java/org/example/Main.java
+++ array-diff-java/ArrayDiff/src/main/java/org/example/Main.java
@@ -1,19 +1,39 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] arr1 = {1, 2};
+        int[] arr2 = {1, 2};
+    /*-------------------------------------*/
+        int[] arr10 = {1, 2, 2};
+        int[] arr20 = {1};
+    /*-------------------------------------*/
+        int[] arr100 = {1, 2, 2};
+        int[] arr200 = {2};
 
-        public static int[] arrayDiff(int[] arr1, int[] arr2) {
-//            int[] arr1 = {1, 2};
-//            int[] arr2 = {1};
-            boolean evaluator = false;  // arr1_element != arr2_element
+    /*-------------------------------------*/
+        int[] arr110 = {};
+        int[] arr220 = {2};
 
-            for (int i : arr1) {
-                while (!evaluator) {
-                    if ()
-                }
-            }
-            return new int[] = {1};
-        }
+    /*-------------------------------------*/
+        int[] arr111 = {1, 2};
+        int[] arr222 = {};
+
+        int[] result1 = Solution.arrayDiff(arr1, arr2);
+        System.out.println(Arrays.toString(result1));
+
+        int[] result10 = Solution.arrayDiff(arr10, arr20);
+        System.out.println(Arrays.toString(result10));
+
+        int[] result100 = Solution.arrayDiff(arr100, arr200);
+        System.out.println(Arrays.toString(result100));
+
+        int[] result110 = Solution.arrayDiff(arr110, arr220);
+        System.out.println(Arrays.toString(result110));
+
+        int[] result111 = Solution.arrayDiff(arr111, arr222);
+        System.out.println(Arrays.toString(result111));
     }
 }
---
+++ array-diff-java/ArrayDiff/src/main/java/org/example/Solution.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.*;
+
+/**
+ * Your goal in this kata is to implement a difference function, which subtracts
+ * one list from another and returns the result.
+ * <p>
+ * It should remove all values from list a, which are present in list b keeping their order.
+ * <p>
+ * Kata.arrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] {2}
+ * If a value is present in b, all of its occurrences must be removed from the other:
+ * <p>
+ * Kata.arrayDiff(new int[] {1, 2, 2, 2, 3}, new int[] {2}) => new int[] {1, 3}
+ */
+public class Solution {
+
+    public static int[] arrayDiff(int[] arr1, int[] arr2) {
+        if (arr1.length == 0 || arr2.length == 0) {
+            return arr1;
+        }
+        List<Integer> arrList2 = new ArrayList<>();
+        for (int i : arr2) {
+            arrList2.add(i);
+        }
+        return Arrays.stream(arr1)
+                .filter(n -> !(arrList2.contains(n)))
+                .toArray();
+    }
+}
