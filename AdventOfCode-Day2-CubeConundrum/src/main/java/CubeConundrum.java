d9d89ae8962db8a8173c1f5c753e2d9c16379d34
kokakarova <kostadinka.karova@appliedtechnology.se>
2024-03-04 22:17:02 +0100

    solve the second part of the puzzle

--- AdventOfCode-Day2-CubeConundrum/src/main/java/CubeConundrum.java
+++ AdventOfCode-Day2-CubeConundrum/src/main/java/CubeConundrum.java
@@ -1,7 +1,7 @@
 public class CubeConundrum {
     public static void main(String[] args) {
         final String fileName = "puzzleInput.txt";
-//        System.out.println(PossibleGame.readPuzzle(fileName));
+        System.out.println(PossibleGame.readPuzzle(fileName));
         System.out.println(FewestCubes.readPuzzle(fileName));
     }
 }
--- AdventOfCode-Day2-CubeConundrum/src/main/java/FewestCubes.java
+++ AdventOfCode-Day2-CubeConundrum/src/main/java/FewestCubes.java
@@ -3,15 +3,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-//game1: green 14, blue 7, red 1 = 98
-//game2: green 3, blue 7, red 4 = 84
-//game3: green 12, blue 10, red 6 = 720
-//        = 902
-
 public class FewestCubes {
 
     public static int readPuzzle(String fileName) {
-        int result = 1;
+        int result = 0;
         try (InputStream file = FewestCubes.class.getClassLoader().getResourceAsStream(fileName);
         ) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(file));
@@ -36,10 +31,6 @@ public class FewestCubes {
 
         for (int i = 0; i < gameArray.length; i++) {
             String[] gameSet = gameArray[i].split(" ");
-//            System.out.println("gameLine length= " + gameArray.length);
-//            System.out.println("i = " + i + " Game Array at i = " + gameArray[i]);
-//            System.out.println("gameSet = " + Arrays.toString(gameSet));
-//            System.out.println("numAndColorSet[" + i + "] = " + gameSet[i]);
             if (i == 0) {
                 continue;
             }
@@ -50,7 +41,6 @@ public class FewestCubes {
                 } else {
                     highestRed = Math.max(highestRed, Integer.parseInt(gameSet[1]));
                 }
-//                System.out.println("lowestRed = " + lowestRed);
             }
             if (gameSet[2].contains("green")) {
                 countGreen++;
@@ -59,7 +49,6 @@ public class FewestCubes {
                 } else {
                     highestGreen = Math.max(highestGreen, Integer.parseInt(gameSet[1]));
                 }
-//                System.out.println("lowestGreen = " + lowestGreen);
             }
             if (gameSet[2].contains("blue")) {
                 countBlue++;
@@ -68,30 +57,9 @@ public class FewestCubes {
                 } else {
                     highestBlue = Math.max(highestBlue, Integer.parseInt(gameSet[1]));
                 }
-//                System.out.println("highestBlue = " + highestBlue);
             }
         }
-//        System.out.println("lowestRed = " + lowestRed);
-//        System.out.println("lowestGreen = " + lowestGreen);
-//        System.out.println("highestBlue = " + highestBlue);
         return (highestRed * highestGreen * highestBlue);
     }
-
-//    public static int checkRed(String reds) {
-//        System.out.println("reds = " + reds);
-//        int result = 1;
-//        return Math.min(result, Integer.parseInt(reds));
-//    }
-//
-//    public static int checkGreen(String greens) {
-//        int result = 1;
-//        return Math.min(result, Integer.parseInt(greens));
-//    }
-//
-//    public static int checkBlue(String blues) {
-//        System.out.println("blues = " + blues);
-//        int result = 1;
-//        return Math.min(result, Integer.parseInt(blues));
-//    }
 }
 
