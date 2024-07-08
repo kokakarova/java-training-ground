dfac5cc0f7f7f9c966d65a1a354b9466b9684eef
kokakarova <kostadinkakarova@gmail.com>
2024-07-08 13:17:18 +0200

    remove unused imports and code

--- AdventOfCode-Day3-GearRatios/GearRatios/src/main/java/org/example/MissingParts.java
+++ AdventOfCode-Day3-GearRatios/GearRatios/src/main/java/org/example/MissingParts.java
@@ -3,8 +3,6 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 
 public class MissingParts {
@@ -23,14 +21,4 @@ public class MissingParts {
         return indexes;
     }
 }
-//    }
-//    public static /*int[][]*/ Object[] getNumberIndexes(String line) {
-//        System.out.println("line = " + line);
-//        System.out.println("line CharArray= " + Arrays.toString(line.toCharArray()));
-//        Object[] testRes = Stream.of(line)
-//                .filter(c -> Character.isDigit(c.toCharArray()[0]))
-//                .map(c -> c.indexOf(c)).toArray();
-//        System.out.println("Arrays.toString(testRes) = " + Arrays.toString(testRes));
-//        return testRes;
-//    }
     
