64b71d065b577c3dc76d97e0a88dedc2302a4ba3
kokakarova <kostadinkakarova@gmail.com>
2024-07-08 17:58:50 +0200

    update decaMultiplier

--- AdventOfCode-Day3-GearRatios/GearRatios/src/main/java/org/example/MissingParts.java
+++ AdventOfCode-Day3-GearRatios/GearRatios/src/main/java/org/example/MissingParts.java
@@ -1,24 +1,33 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import lombok.Data;
+
+import java.util.HashMap;
 import java.util.List;
 
-
+@Data
 public class MissingParts {
 
-    public static List<Integer> getNumberIndexes(String line) {
-        System.out.println("line = " + line);
-        System.out.println("line CharArray= " + Arrays.toString(line.toCharArray()));
-        List<Integer> indexes = new ArrayList<>();
-        char[] lineToCharArray = line.toCharArray();
-        for (int i = 0; i < lineToCharArray.length; i++) {
-            if (Character.isDigit(lineToCharArray[i])) {
-                indexes.add(i);
-            }
-        }
-        System.out.println("indexes.list = " + indexes);
-        return indexes;
+    private HashMap<Integer, Integer> specialChars = new HashMap<>();
+    private HashMap<Integer, List<Integer>> numbers = new HashMap<>();
+
+//    public static List<Integer> getNumberIndexes(String line) {
+//        System.out.println("line = " + line);
+//        System.out.println("line CharArray= " + Arrays.toString(line.toCharArray()));
+//        List<Integer> indexes = new ArrayList<>();
+//        char[] lineToCharArray = line.toCharArray();
+//        for (int i = 0; i < lineToCharArray.length; i++) {
+//            if (Character.isDigit(lineToCharArray[i])) {
+//                indexes.add(i);
+//            }
+//        }
+//        System.out.println("indexes.list = " + indexes);
+//        return indexes;
+//    }
+
+    public static int decaMultiplier(int value, int decaValue) {
+        // decaValue is 1, 2 (decades), 3(hundreds), 4(thousands) ...
+        return (int) (value * Math.pow(10, decaValue - 1));
     }
 }
     
