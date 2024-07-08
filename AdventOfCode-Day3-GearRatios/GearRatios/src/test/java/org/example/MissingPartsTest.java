b296386bf668b4f042d47a967fbd13eac7be78c1
kokakarova <kostadinkakarova@gmail.com>
2024-07-08 17:58:18 +0200

    update tests

--- AdventOfCode-Day3-GearRatios/GearRatios/src/test/java/org/example/MissingPartsTest.java
+++ AdventOfCode-Day3-GearRatios/GearRatios/src/test/java/org/example/MissingPartsTest.java
@@ -9,11 +9,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MissingPartsTest {
 
+    MissingParts missingParts = new MissingParts();
+
     @Test
-    void should() {
-        String testString = "..38.143...1.......";
-        int[][] expectedResult = new int[][]{{2, 3}, {5, 6, 7}, {11}};
-        List<Integer> actualResult = MissingParts.getNumberIndexes(testString);
-        assertEquals(2, actualResult.get(0));
+    void shouldRetrun7() {
+        int value = 7;
+        int decaPlace = 1;
+        assertEquals(7, MissingParts.decaMultiplier(value, decaPlace));
     }
+
+    @Test
+    void shouldRetrun70() {
+        int value = 7;
+        int decaPlace = 2;
+        assertEquals(70, MissingParts.decaMultiplier(value, decaPlace));
+    }
+
+//    @Test
+//    void shouldRetrun0() {
+//        String testString = "..38.143...1.......";
+//        assertEquals(3, missingParts.);
+//    }
+
 }
