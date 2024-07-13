2e0783d5e88526f05e24cb179f547068bc063f8e
kokakarova <kostadinkakarova@gmail.com>
2024-07-13 02:06:58 +0200

    break down tests

--- AdventOfCode-Day3-GearRatios/GearRatios/src/test/java/org/example/MissingPartsTest.java
+++ AdventOfCode-Day3-GearRatios/GearRatios/src/test/java/org/example/MissingPartsTest.java
@@ -2,11 +2,15 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MissingPartsTest {
 
     CalculateMissingParts missingParts = new CalculateMissingParts();
+    Numbers numbers = new Numbers();
+    SpecialChars specialChars = new SpecialChars();
 
 //    @Test
 //    void shouldReturn7() {
@@ -36,5 +40,86 @@ class MissingPartsTest {
 //        assertEquals(2, actualResult.size());
 //    }
 
+    
+//    @Test
+//    void calculateMissingParts() {
+//        HashMap<Integer, Integer> innerMap = new HashMap<>();
+//        innerMap.put(1, 1);
+//        innerMap.put(2, 1);
+//        innerMap.put(3, 1);
+//        HashMap<Integer, HashMap<Integer, Integer>> outerMap = new HashMap<>();
+//        outerMap.put(1, innerMap);
+//        System.out.println("(before) numbersMap(test) = " + numbers.getNumbersMap());
+//        numbers.setNumbersMap(outerMap);
+//        System.out.println("(after) numbersMap(test) = " + numbers.getNumbersMap());
+//        int actualResult = missingParts.addNumsFromPrevLine(1, 3);
+//        assertEquals(111, actualResult);
+//    }
 
+    @Test
+    void shouldGet11466ForTotalSum() throws IOException {
+        int initialSumVal = (int) missingParts.getTotalSum();
+        String fileName = "puzzleFileTest.txt";
+        missingParts.readFromFile(fileName);
+        int endSumVal = (int) missingParts.getTotalSum();
+        int expectedResult = 11466;
+        int actualResult = endSumVal - initialSumVal;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldGet794ForTotalSum() throws IOException {
+        int initialSumVal = (int) missingParts.getTotalSum();
+        String fileName = "puzzleFileTestSpecialCharInPrevLine.txt";
+        missingParts.readFromFile(fileName);
+        int endSumVal = (int) missingParts.getTotalSum();
+        int expectedResult = 794;
+        int actualResult = endSumVal - initialSumVal;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldGet850ForTotalSum() throws IOException {
+        int initialSumVal = (int) missingParts.getTotalSum();
+        String fileName = "puzzleFileTestSpecialChar.txt";
+        missingParts.readFromFile(fileName);
+        int endSumVal = (int) missingParts.getTotalSum();
+        int expectedResult = 850;
+        int actualResult = endSumVal - initialSumVal;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldGet100ForTotalSum() throws IOException {
+        int initialSumVal = (int) missingParts.getTotalSum();
+        String fileName = "puzzleFileTestSpecialCharBeforeNum.txt";
+        missingParts.readFromFile(fileName);
+        int endSumVal = (int) missingParts.getTotalSum();
+        int expectedResult = 100;
+        int actualResult = endSumVal - initialSumVal;
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+    @Test
+    void shouldGet434ForTotalSum() throws IOException {
+        int initialSumVal = (int) missingParts.getTotalSum();
+        String fileName = "puzzleFileTestSpecialCharAfterNum.txt";
+        missingParts.readFromFile(fileName);
+        int endSumVal = (int) missingParts.getTotalSum();
+        int expectedResult = 434;
+        int actualResult = endSumVal - initialSumVal;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldGet5760ForTotalSum() throws IOException {
+        int initialSumVal = (int) missingParts.getTotalSum();
+        String fileName = "puzzleFileTest-1.txt";
+        missingParts.readFromFile(fileName);
+        int endSumVal = (int) missingParts.getTotalSum();
+        int expectedResult = 5760;
+        int actualResult = endSumVal - initialSumVal;
+        assertEquals(expectedResult, actualResult);
+    }
 }
