586afdca6e5896b288c2445b972dd0c1dc7dd3d8
kokakarova <kostadinkakarova@gmail.com>
2024-07-19 15:20:25 +0200

    call methods from main

--- AdventOfCode-Day4-Scratchcards/Scratchcards/src/main/java/org/scratchcard/Main.java
+++ AdventOfCode-Day4-Scratchcards/Scratchcards/src/main/java/org/scratchcard/Main.java
@@ -1,7 +1,11 @@
 package org.scratchcard;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+    Winnings winnings = new Winnings();
         System.out.println("Hello world!");
+        winnings.readFromFile("puzzleExample.txt");
     }
 }
