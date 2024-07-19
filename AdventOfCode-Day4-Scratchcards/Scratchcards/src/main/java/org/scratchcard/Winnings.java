ec3a1a4eccb1334c4895c1a477dbc48ef5ace84a
kokakarova <kostadinkakarova@gmail.com>
2024-07-19 13:52:49 +0200

    create initial class

---
+++ AdventOfCode-Day4-Scratchcards/Scratchcards/src/main/java/org/scratchcard/Winnings.java
@@ -0,0 +1,5 @@
+package org.scratchcard;
+
+public class Winnings {
+
+}
---
+++ AdventOfCode-Day4-Scratchcards/Scratchcards/src/test/java/org/scratchcard/WinningsTest.java
@@ -0,0 +1,14 @@
+package org.scratchcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinningsTest {
+
+    @Test
+    void shouldGet13ForPuzzleExample() {
+        int expectedResult = 13;
+    }
+
+}
