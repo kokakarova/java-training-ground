c88fa867c9d9fc166fc4f5043b4ce1dc974ae7c6
kokakarova <kostadinkakarova@gmail.com>
2024-07-19 08:58:19 +0200

    pass the tests

--- AdventOfCode-Day3-GearRatios/GearRatios/src/test/java/org/example/BrokenGearsTest.java
+++ AdventOfCode-Day3-GearRatios/GearRatios/src/test/java/org/example/BrokenGearsTest.java
@@ -10,13 +10,36 @@ class BrokenGearsTest {
     BrokenGears bg = new BrokenGears();
 
     @Test
-    void shouldReturn_600_forPuzzlePart2Simple() throws IOException {
+    void shouldReturn_600_forPuzzlePart2HandleGearSymbol() throws IOException {
         int initialSumVal = (int) bg.getTotalSum();
-        String fileName = "puzzlePart2Simple.txt";
+        String fileName = "puzzlePart2HandleGearSymbol.txt";
         bg.readFromFile(fileName);
         int endSumVal = (int) bg.getTotalSum();
         int expectedResult = 600;
         int actualResult = endSumVal - initialSumVal;
         assertEquals(expectedResult, actualResult);
     }
+
+    @Test
+    void shouldReturn_1000_forPuzzlePart2HandleNumbers() throws IOException {
+        int initialSumVal = (int) bg.getTotalSum();
+        String fileName = "puzzlePart2HandleNumbers.txt";
+        bg.readFromFile(fileName);
+        int endSumVal = (int) bg.getTotalSum();
+        int expectedResult = 1000;
+        int actualResult = endSumVal - initialSumVal;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void shouldReturn_467835_forPuzzleExample() throws IOException {
+        int initialSumVal = (int) bg.getTotalSum();
+        String fileName = "puzzleExample.txt";
+        bg.readFromFile(fileName);
+        int endSumVal = (int) bg.getTotalSum();
+        int expectedResult = 467835;
+        int actualResult = endSumVal - initialSumVal;
+        assertEquals(expectedResult, actualResult);
+    }
+
 }
