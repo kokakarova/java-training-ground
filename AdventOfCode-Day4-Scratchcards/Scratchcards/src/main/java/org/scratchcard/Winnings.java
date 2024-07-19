cbf93d9cb4250279ff847f29af54c5b68cfae12d
kokakarova <kostadinkakarova@gmail.com>
2024-07-19 15:20:55 +0200

    get winningNumbers and numbersYouHave in two separate arrays

--- AdventOfCode-Day4-Scratchcards/Scratchcards/src/main/java/org/scratchcard/Winnings.java
+++ AdventOfCode-Day4-Scratchcards/Scratchcards/src/main/java/org/scratchcard/Winnings.java
@@ -1,5 +1,28 @@
 package org.scratchcard;
 
-public class Winnings {
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Winnings implements FileReader {
+
+    @Override
+    public void readFromFile(String fileName) throws IOException {
+        FileReader.super.readFromFile(fileName, "winnings");
+    }
+
+    public void processLine(String line) {
+        String splitByColon = line.split(":")[1];
+        String[] splitByPipe = splitByColon.split("\\|");
+        int[] winningNumbers = getNumbersArray(splitByPipe[0].split(" "));
+        System.out.println("winningNumbers = " + Arrays.toString(winningNumbers));
+        int[] numbersYouHave = getNumbersArray(splitByPipe[1].split(" "));
+        System.out.println("numbersYouHave = " + Arrays.toString(numbersYouHave));
+    }
+
+    private int[] getNumbersArray(String[] inputString) {
+        return Stream.of(inputString).filter(n -> !n.isEmpty()).mapToInt(Integer::parseInt).toArray();
+    }
 
 }
