3a7f314e975e1b85eb8ed9b1b1d80e5ff4d6c28e
kokakarova <kostadinkakarova@gmail.com>
2024-07-27 12:57:42 +0200

    add tracker for running time

--- AdventOfCode-Day6-BoatRace/src/main/java/org/boatRace/Main.java
+++ AdventOfCode-Day6-BoatRace/src/main/java/org/boatRace/Main.java
@@ -2,6 +2,8 @@ package org.boatRace;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final long start = System.currentTimeMillis();
+        final long end = System.currentTimeMillis();
+        System.out.println("The program was running: " + (end-start) + "ms.");
     }
 }
