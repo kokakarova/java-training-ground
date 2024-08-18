f0d92826c3df93392f1d4e097ddb45e177bceab7
kokakarova <kostadinkakarova@gmail.com>
2024-08-19 00:25:01 +0200

    add run time counter

--- AdventOfCode-Day8-HauntedWasteland/src/main/java/org/camleCards/Main.java
+++ AdventOfCode-Day8-HauntedWasteland/src/main/java/org/camleCards/Main.java
@@ -3,7 +3,12 @@ package org.camleCards;
 public class Main {
     public static void main(String[] args) {
         Wasteland w = new Wasteland();
-        w.readFromFile("puzzle.txt");
-        System.out.println("ZZZ was found in " + w.findZZZ() + " steps");
+        final long start = System.currentTimeMillis();
+        w.readFromFile("puzzle.txt", 2);
+//        System.out.println("ZZZ was found in " + w.findZZZ() + " steps");
+        System.out.println("ZZZ was found in " + w.findAllZNodes() + " steps");
+        final long end = System.currentTimeMillis();
+        System.out.println("* * * * * * * * * * * * * * * * ");
+        System.out.println("The program was running: " + (end - start) + "ms.");
     }
 }
