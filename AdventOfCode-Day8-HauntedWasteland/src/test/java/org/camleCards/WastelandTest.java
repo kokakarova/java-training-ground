fcff8a863af10dd70b1b28cb55f166b9bdf82488
kokakarova <kostadinkakarova@gmail.com>
2024-08-19 00:25:39 +0200

    create and pass test for part 2

--- AdventOfCode-Day8-HauntedWasteland/src/test/java/org/camleCards/WastelandTest.java
+++ AdventOfCode-Day8-HauntedWasteland/src/test/java/org/camleCards/WastelandTest.java
@@ -7,20 +7,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class WastelandTest {
     Wasteland wasteland = new Wasteland();
     @Test
-    void shouldReturn_2_PuzzleExample1() {
-        wasteland.readFromFile("puzzleExample1.txt");
+    void shouldReturn_2_PuzzleExamplePart1_1() {
+        wasteland.readFromFile("puzzleExamplePart1_1.txt", 1);
         int expected = 2;
         int actual = wasteland.findZZZ();
         assertEquals(expected, actual);
     }
 
     @Test
-    void shouldReturn_6_PuzzleExample2() {
-        wasteland.readFromFile("puzzleExample2.txt");
+    void shouldReturn_6_PuzzleExamplePart1_2() {
+        wasteland.readFromFile("puzzleExamplePart1_2.txt",1 );
         int expected = 6;
         int actual = wasteland.findZZZ();
         System.out.println("actual = " + actual);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn_6_PuzzleExamplePart2_1() {
+        wasteland.readFromFile("puzzleExamplePart2_1.txt", 2);
+        int expected = 6;
+        int actual = wasteland.findAllZNodes();
+        assertEquals(expected, actual);
+
     }
 
 }
