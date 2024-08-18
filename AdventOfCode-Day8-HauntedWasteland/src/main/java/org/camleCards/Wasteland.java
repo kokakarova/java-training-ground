7dc76bd7c027b82a56fc36eba7852df3229c94d4
kokakarova <kostadinkakarova@gmail.com>
2024-08-18 10:41:06 +0200

    WIP - start solving for part 2

--- AdventOfCode-Day8-HauntedWasteland/src/main/java/org/camleCards/Wasteland.java
+++ AdventOfCode-Day8-HauntedWasteland/src/main/java/org/camleCards/Wasteland.java
@@ -6,15 +6,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Data
 public class Wasteland {
     char[] directions = "LLRLRRRLLLRLRRLRRRLRLRRLRLRLRRRLRRRLRLRLRRLLRRRLRRLRRLLRLRRRLRLRLLRRRLLRRRLRLRRRLRRRLRRRLLLRRRLRRLRRLRLRRLRLRRRLRLRRLRLRLRRRLRLLLRRRLLLRLRRRLRLRRLRLRLRLRRLRRLRRLRLRRRLRRRLRRLRRRLRRLRRLRRRLLRLRRLLLRRLRRLRLRLLLRRLRRLRRRLRRLLRLRRRLRRRLRRLRRLRLRRLRLRRRLRRLRRRLLRRRLRLRLLLRRRLLLRRLLRRLRLRRLRLLLRRRR".toCharArray();
-//    char[] directions = "RL".toCharArray();
+    //    char[] directions = "RL".toCharArray();
 //    char[] directions = "LLR".toCharArray();
     Map<String, String[]> locationsMap = new HashMap<>();
+    List<String> startingNodes = new ArrayList<>();
+    List<String> endingNodes = new ArrayList<>();
 
     public void readFromFile(String fileName) {
         try (InputStream file = Main.class.getClassLoader().getResourceAsStream(fileName)) {
@@ -25,7 +26,16 @@ public class Wasteland {
                 processLine(line);
                 lineCounter++;
             }
-            System.out.println("directions.length = " + directions.length);
+            System.out.println(startingNodes.size() + " starting Nodes");
+            for (String node: startingNodes) {
+                System.out.println(node);
+            }
+            System.out.println(endingNodes.size() + " ending Nodes");
+            for (String node: endingNodes) {
+                System.out.println(node);
+            }
+//            System.out.println("directions.length = " + directions.length);
+//            System.out.println("locationsMap size = " + locationsMap.size());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -36,28 +46,40 @@ public class Wasteland {
         String left = line.substring(7, 10);
         String right = line.substring(12, line.length() - 1);
         locationsMap.put(location, new String[]{left, right});
+        if (location.endsWith("A")) {
+            startingNodes.add(location);
+        }
+        if (location.endsWith("Z")) {
+            endingNodes.add(location);
+        }
     }
 
     public int findZZZ() {
         int steps = 0;
         int left = 0;
         int right = 1;
-        String currentLocation = "BNR";
-//        String currentLocation = "AAA";
+        String currentLocation = "AAA";
 
         for (int i = 0; i < directions.length; i++) {
             steps++;
             currentLocation = directions[i] == 'L' ? locationsMap.get(currentLocation)[left]
                     : locationsMap.get(currentLocation)[right];
             if (currentLocation.equals("ZZZ")) {
-                i = locationsMap.size();
+                return steps;
             } else if (i == directions.length - 1) {
-                i = 0;
+                i = -1;
             }
         }
 
         return steps;
     }
 
+    public int findAllZNodes() {
+        int steps = 0;
+        int left = 0;
+        int right = 1;
+
+        return steps;
+    }
 
 }
