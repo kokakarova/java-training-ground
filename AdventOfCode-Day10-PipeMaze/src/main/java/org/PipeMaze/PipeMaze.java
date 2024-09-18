de9556fc81beb82776f9c812a16554455e70295a
kokakarova <kostadinkakarova@gmail.com>
2024-09-18 23:29:36 +0200

    save signs in Set<Float> instead of map

--- AdventOfCode-Day10-PipeMaze/src/main/java/org/PipeMaze/PipeMaze.java
+++ AdventOfCode-Day10-PipeMaze/src/main/java/org/PipeMaze/PipeMaze.java
@@ -6,20 +6,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Data
 public class PipeMaze {
-    Map<Integer, List<Integer>> pipeSigns = new HashMap<>();
-    Map<Integer, List<Integer>> dashSigns = new HashMap<>();
-    Map<Integer, List<Integer>> lSigns = new HashMap<>();
-    Map<Integer, List<Integer>> jSigns = new HashMap<>();
-    Map<Integer, List<Integer>> sevenSigns = new HashMap<>();
-    Map<Integer, List<Integer>> fSigns = new HashMap<>();
-    Map<Integer, Integer> startingPosition = new HashMap<>();
+    Set<Float> pipeVertexes = new TreeSet<>();
+    Set<Float> dashVertexes = new TreeSet<>();
+    Set<Float> lSignVertexes = new TreeSet<>();
+    Set<Float> jSignVertexes = new TreeSet<>();
+    Set<Float> sevenSignVertexes = new TreeSet<>();
+    Set<Float> fSignVertexes = new TreeSet<>();
+    float startingPosition;
 
     public void readFromFile(String fileName, int part) {
         int lineNumber = 0;
@@ -31,6 +28,8 @@ public class PipeMaze {
                 char[] lineChars = line.toCharArray();
                 allocateCharacters(lineNumber, lineChars);
             }
+            System.out.println("pipeVertexes.size() = " + pipeVertexes.size());
+            System.out.println("pipeVertexes = " + pipeVertexes);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,48 +37,21 @@ public class PipeMaze {
 
     private void allocateCharacters(int lineNumber, char[] lineChars) {
         for (int i = 0; i < lineChars.length; i++) {
+            float locationValue = (float)lineNumber + (float) (i * 1.0) / 1000;
             switch (lineChars[i]) {
-                case 'S' -> startingPosition.put(lineNumber, i);
-                case '|' -> addToSignsMap("pipe", lineNumber, i);
-                case '-' -> addToSignsMap("dash", lineNumber, i);
-                case 'L' -> addToSignsMap("L", lineNumber, i);
-                case 'J' -> addToSignsMap("J", lineNumber, i);
-                case '7' -> addToSignsMap("seven", lineNumber, i);
-                case 'F' -> addToSignsMap("F", lineNumber, i);
+                case 'S' -> startingPosition = locationValue;
+                case '|' -> {
+                    pipeVertexes.add(locationValue);
+                }
+                case '-' -> dashVertexes.add(locationValue);
+                case 'L' -> lSignVertexes.add(locationValue);
+                case 'J' -> jSignVertexes.add(locationValue);
+                case '7' -> sevenSignVertexes.add(locationValue);
+                case 'F' -> fSignVertexes.add(locationValue);
                 default -> {
                     continue;
                 }
             }
-        }
-    }
-
-    private void addToSignsMap(String signType, int lineNumber, int i) {
-        switch (signType) {
-            case "pipe" -> {
-                pipeSigns.putIfAbsent(lineNumber, new ArrayList<>());
-                pipeSigns.get(lineNumber).add(i);
-            }
-            case "dash" -> {
-                dashSigns.putIfAbsent(lineNumber, new ArrayList<>());
-                dashSigns.get(lineNumber).add(i);
-            }
-            case "L" -> {
-                lSigns.putIfAbsent(lineNumber, new ArrayList<>());
-                lSigns.get(lineNumber).add(i);
-            }
-            case "J" -> {
-                jSigns.putIfAbsent(lineNumber, new ArrayList<>());
-                jSigns.get(lineNumber).add(i);
-            }
-            case "seven" -> {
-                sevenSigns.putIfAbsent(lineNumber, new ArrayList<>());
-                sevenSigns.get(lineNumber).add(i);
-            }
-            case "F" -> {
-                fSigns.putIfAbsent(lineNumber, new ArrayList<>());
-                fSigns.get(lineNumber).add(i);
-            }
-            default -> {}
         }
     }
 
