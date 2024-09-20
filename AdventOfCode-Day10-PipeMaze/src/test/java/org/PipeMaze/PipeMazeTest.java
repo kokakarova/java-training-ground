3e15baf70cf4ddcc7778351ad0ddfe755a00cf11
kokakarova <kostadinkakarova@gmail.com>
2024-09-20 22:23:44 +0200

    add one more test and a test puzzle

--- AdventOfCode-Day10-PipeMaze/src/test/java/org/PipeMaze/PipeMazeTest.java
+++ AdventOfCode-Day10-PipeMaze/src/test/java/org/PipeMaze/PipeMazeTest.java
@@ -8,11 +8,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PipeMazeTest {
 
-    String fileName = "puzzleExample.txt";
+
     PipeMaze pipeMaze = new PipeMaze();
 
     @Test
     void shouldSave_7Pipe_5Dash_4Ls_2Fs_4Sevens_2Js() {
+        String fileName = "puzzleExample.txt";
         int expectedPipes = 7;
         int expectedDashes = 5;
         int expectedLs = 4;
@@ -20,17 +21,34 @@ class PipeMazeTest {
         int expectedSevens = 4;
         int expectedJs = 2;
         pipeMaze.readFromFile(fileName, 1);
-        int actualPipes = pipeMaze.pipeSigns.values().stream().mapToInt(List::size).sum();
-        int actualDashes = pipeMaze.dashSigns.values().stream().mapToInt(List::size).sum();
-        int actualLs = pipeMaze.lSigns.values().stream().mapToInt(List::size).sum();
-        int actualFs = pipeMaze.fSigns.values().stream().mapToInt(List::size).sum();
-        int actualSevens = pipeMaze.sevenSigns.values().stream().mapToInt(List::size).sum();
-        int actualJs = pipeMaze.jSigns.values().stream().mapToInt(List::size).sum();
+        int actualPipes = pipeMaze.pipeVertexes.values().stream().mapToInt(List::size).sum();
+        int actualDashes = pipeMaze.dashVertexes.values().stream().mapToInt(List::size).sum();
+        int actualLs = pipeMaze.lSignVertexes.values().stream().mapToInt(List::size).sum();
+        int actualFs = pipeMaze.fSignVertexes.values().stream().mapToInt(List::size).sum();
+        int actualSevens = pipeMaze.sevenSignVertexes.values().stream().mapToInt(List::size).sum();
+        int actualJs = pipeMaze.jSignVertexes.values().stream().mapToInt(List::size).sum();
         assertEquals(expectedPipes, actualPipes);
         assertEquals(expectedDashes, actualDashes);
         assertEquals(expectedLs, actualLs);
         assertEquals(expectedFs, actualFs);
         assertEquals(expectedSevens, actualSevens);
         assertEquals(expectedJs, actualJs);
+    }
+
+    @Test
+    void shouldReturn_4_For_PuzzleExample() {
+        String fileName = "puzzleExample.txt";
+        long expectedResult = 4;
+        pipeMaze.readFromFile(fileName, 1);
+        long actualResult = pipeMaze.getFurthestVertex();
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    void shouldReturn_8_For_PuzzleExample2() {
+        String fileName = "puzzleExample2.txt";
+        long expectedResult = 8;
+        pipeMaze.readFromFile(fileName, 1);
+        long actualResult = pipeMaze.getFurthestVertex();
+        assertEquals(expectedResult, actualResult);
     }
 }
