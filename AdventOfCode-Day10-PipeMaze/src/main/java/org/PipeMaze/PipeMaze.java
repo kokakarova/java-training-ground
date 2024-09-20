0504f8688824d499a95038ea2bebc5e66a41841e
kokakarova <kostadinkakarova@gmail.com>
2024-09-20 22:25:23 +0200

    edit methods to get correct answer for part 1

--- AdventOfCode-Day10-PipeMaze/src/main/java/org/PipeMaze/PipeMaze.java
+++ AdventOfCode-Day10-PipeMaze/src/main/java/org/PipeMaze/PipeMaze.java
@@ -10,13 +10,19 @@ import java.util.*;
 
 @Data
 public class PipeMaze {
-    Set<Float> pipeVertexes = new TreeSet<>();
-    Set<Float> dashVertexes = new TreeSet<>();
-    Set<Float> lSignVertexes = new TreeSet<>();
-    Set<Float> jSignVertexes = new TreeSet<>();
-    Set<Float> sevenSignVertexes = new TreeSet<>();
-    Set<Float> fSignVertexes = new TreeSet<>();
-    float startingPosition;
+    Map<Integer, List<Integer>> pipeVertexes = new HashMap<>();
+    Map<Integer, List<Integer>> dashVertexes = new HashMap<>();
+    Map<Integer, List<Integer>> lSignVertexes = new HashMap<>();
+    Map<Integer, List<Integer>> jSignVertexes = new HashMap<>();
+    Map<Integer, List<Integer>> sevenSignVertexes = new HashMap<>();
+    Map<Integer, List<Integer>> fSignVertexes = new HashMap<>();
+    NextStep currentStep = new NextStep();
+//    float startingPosition;
+
+    private static final float GO_NORTH = -1.0F;
+    private static final float GO_SOUTH = 1.0F;
+    private static final float GO_EAST = 0.001F;
+    private static final float GO_WEST = -0.001F;
 
     public void readFromFile(String fileName, int part) {
         int lineNumber = 0;
@@ -28,7 +34,7 @@ public class PipeMaze {
                 char[] lineChars = line.toCharArray();
                 allocateCharacters(lineNumber, lineChars);
             }
-            System.out.println("pipeVertexes.size() = " + pipeVertexes.size());
+            System.out.println("dashVertexes = " + dashVertexes);
             System.out.println("pipeVertexes = " + pipeVertexes);
         } catch (IOException e) {
             e.printStackTrace();
@@ -37,22 +43,191 @@ public class PipeMaze {
 
     private void allocateCharacters(int lineNumber, char[] lineChars) {
         for (int i = 0; i < lineChars.length; i++) {
-            float locationValue = (float)lineNumber + (float) (i * 1.0) / 1000;
             switch (lineChars[i]) {
-                case 'S' -> startingPosition = locationValue;
-                case '|' -> {
-                    pipeVertexes.add(locationValue);
+                case 'S' -> {
+                    currentStep.setLine(lineNumber);
+                    currentStep.setPosition(i);
+                    currentStep.setComingFrom("east");
                 }
-                case '-' -> dashVertexes.add(locationValue);
-                case 'L' -> lSignVertexes.add(locationValue);
-                case 'J' -> jSignVertexes.add(locationValue);
-                case '7' -> sevenSignVertexes.add(locationValue);
-                case 'F' -> fSignVertexes.add(locationValue);
+                case '|' -> {
+                    pipeVertexes.putIfAbsent(lineNumber, new ArrayList<>());
+                    pipeVertexes.get(lineNumber).add(i);
+                }
+                case '-' -> {
+                    dashVertexes.putIfAbsent(lineNumber, new ArrayList<>());
+                    dashVertexes.get(lineNumber).add(i);
+                }
+                case 'L' -> {
+                    lSignVertexes.putIfAbsent(lineNumber, new ArrayList<>());
+                    lSignVertexes.get(lineNumber).add(i);
+                }
+                case 'J' -> {
+                    jSignVertexes.putIfAbsent(lineNumber, new ArrayList<>());
+                    jSignVertexes.get(lineNumber).add(i);
+                }
+                case '7' -> {
+                    sevenSignVertexes.putIfAbsent(lineNumber, new ArrayList<>());
+                    sevenSignVertexes.get(lineNumber).add(i);
+                }
+                case 'F' -> {
+                    fSignVertexes.putIfAbsent(lineNumber, new ArrayList<>());
+                    fSignVertexes.get(lineNumber).add(i);
+                }
                 default -> {
                     continue;
                 }
             }
         }
+    }
+
+    public long getFurthestVertex() {
+        long stepCounter = 1;
+        int startLine = currentStep.getLine();
+        int startPosition = currentStep.getPosition();
+        System.out.println("stepCounter = " + stepCounter);
+        System.out.println("currentStep = " + currentStep);
+        boolean fullCircleComplete = false;
+
+        while (!fullCircleComplete) {
+            stepCounter++;
+            System.out.println("---------------------------------");
+            System.out.println("stepCounter = " + stepCounter);
+            System.out.println("currentStep = " + currentStep);
+
+            switch (currentStep.comingFrom) {
+                case "west" -> {
+                    currentStep.setPosition(currentStep.getPosition() + 1);
+                    if (atStartingPoint(currentStep, startLine, startPosition, stepCounter)) {
+                        fullCircleComplete = true;
+                        continue;
+                    }
+                    setNextStepComingFromWhenCurrentComingFromWest(currentStep);
+                }
+                case "east" -> {
+                    currentStep.setPosition(currentStep.getPosition() - 1);
+                    if (atStartingPoint(currentStep, startLine, startPosition, stepCounter)) {
+                        fullCircleComplete = true;
+                        continue;
+                    }
+                    setNextStepComingFromWhenCurrentFromEast(currentStep);
+                }
+                case "north" -> {
+                    currentStep.setLine(currentStep.getLine() + 1);
+                    if (atStartingPoint(currentStep, startLine, startPosition, stepCounter)) {
+                        fullCircleComplete = true;
+                        continue;
+                    }
+                    setNextStepComingFromWhenCurrentFromNorth(currentStep);
+                }
+                case "south" -> {
+                    currentStep.setLine(currentStep.getLine() - 1);
+                    if (atStartingPoint(currentStep, startLine, startPosition, stepCounter)) {
+                        fullCircleComplete = true;
+                        continue;
+                    }
+                    setNextStepComingFromWhenCurrentFromSouth(currentStep);
+                }
+                default -> throw new IllegalStateException("Unexpected value: " + currentStep.getComingFrom());
+            }
+
+        }
+        System.out.println("currentStep = " + currentStep);
+        return stepCounter / 2;
+    }
+
+    private boolean atStartingPoint(NextStep nextStep, int startLine, int startPosition, long counter) {
+        return currentStep.getLine() == startLine && currentStep.getPosition() == startPosition && counter > 1;
+    }
+
+    private void setNextStepComingFromWhenCurrentFromSouth(NextStep currentVertex) {
+        if (pipeVertexes.containsKey(currentVertex.getLine())
+                && pipeVertexes.get(currentVertex.getLine()).contains(currentVertex.getPosition())) {
+            System.out.println("found in pipes");
+            currentStep.setComingFrom("south");
+        } else if (sevenSignVertexes.containsKey(currentVertex.getLine())
+                && sevenSignVertexes.get(currentVertex.getLine()).contains(currentVertex.getPosition())) {
+            System.out.println("found in 7s");
+            currentStep.setComingFrom("east");
+        } else if (fSignVertexes.containsKey(currentVertex.getLine())
+                && fSignVertexes.get(currentVertex.getLine()).contains(currentVertex.getPosition())) {
+            System.out.println("found in Fs");
+            currentStep.setComingFrom("west");
+        } else {
+            throw new IllegalStateException("Unexpected value: " + currentStep);
+        }
+    }
+
+    private void setNextStepComingFromWhenCurrentFromNorth(NextStep currentVertex) {
+        if (pipeVertexes.containsKey(currentVertex.getLine())
+                && pipeVertexes.get(currentVertex.getLine()).contains(currentVertex.getPosition())) {
+            System.out.println("found in pipes");
+            currentStep.setComingFrom("north");
+        } else if (lSignVertexes.containsKey(currentVertex.getLine())
+                && lSignVertexes.get(currentVertex.getLine()).contains(currentVertex.getPosition())) {
+            System.out.println("found in Ls");
+            currentStep.setComingFrom("west");
+        } else if (jSignVertexes.containsKey(currentVertex.getLine())
+                && jSignVertexes.get(currentVertex.getLine()).contains(currentVertex.getPosition())) {
+            System.out.println("found in Js");
+            currentStep.setComingFrom("east");
+        } else {
+            throw new IllegalStateException("Unexpected value: " + currentStep);
+        }
+    }
+
+    private void setNextStepComingFromWhenCurrentFromEast(NextStep currentVertex) {
+        if (lSignVertexes.containsKey(currentVertex.getLine())
+                && lSignVertexes.get(currentVertex.getLine()).contains(currentVertex.getPosition())) {
+            System.out.println("found in Ls");
+            currentStep.setComingFrom("south");
+        } else if (fSignVertexes.containsKey(currentVertex.getLine())
+                && fSignVertexes.get(currentVertex.getLine()).contains(currentVertex.getPosition())) {
+            System.out.println("found in Fs");
+            currentStep.setComingFrom("north");
+        } else if (dashVertexes.containsKey(currentVertex.getLine())
+                && dashVertexes.get(currentVertex.getLine()).contains(currentVertex.getPosition())) {
+            System.out.println("found in dashes");
+            currentStep.setComingFrom("east");
+        } else {
+            throw new IllegalStateException("Unexpected value: " + currentStep);
+        }
+    }
+
+
+    private void setNextStepComingFromWhenCurrentComingFromWest(NextStep currentVertex) {
+
+        if (dashVertexes.containsKey(currentVertex.getLine())
+                && dashVertexes.get(currentVertex.getLine()).contains(currentVertex.getPosition())) {
+            System.out.println("found in dashes");
+            currentStep.setComingFrom("west");
+        } else if (jSignVertexes.containsKey(currentVertex.getLine())
+                && jSignVertexes.get(currentVertex.getLine()).contains(currentVertex.getPosition())) {
+            System.out.println("found in Js");
+            currentStep.setComingFrom("south");
+        } else if (sevenSignVertexes.containsKey(currentVertex.getLine())
+                && sevenSignVertexes.get(currentVertex.getLine()).contains(currentVertex.getPosition())) {
+            System.out.println("found in 7s");
+            currentStep.setComingFrom("north");
+        } else {
+            throw new IllegalStateException("Unexpected value: " + currentStep);
+        }
+    }
+
+    public float goWestOrEast(float startPosition, String direction) {
+        int increment = direction.equals("west") ? -1 : 1;
+        String[] floatStringArr = String.valueOf(startPosition).split("\\.");
+        String numToIncreaseString = floatStringArr[1];
+//        int incrementedInt = (Integer.parseInt(numToIncreaseString) + increment);
+
+//        System.out.println("incrementedInt = " + incrementedInt);
+//        float nextPosition = ((float) (Integer.parseInt(numToIncreaseString) + increment) / 1000)
+//                + (float) Integer.parseInt(floatStringArr[0]);
+
+//        System.out.println("numToIncreaseString = " + numToIncreaseString);
+//        System.out.println(" int nextPosition = " + nextPosition);
+
+        return ((float) (Integer.parseInt(numToIncreaseString) + increment) / 1000)
+                + (float) Integer.parseInt(floatStringArr[0]);
     }
 
 }
