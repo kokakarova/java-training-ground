2108c24b6cc741226a736e1eee871a1503ee37f3
kokakarova <kostadinkakarova@gmail.com>
2024-12-08 15:34:11 +0100

    feat: solve for part 1, part 2 is WIP

--- AoC2024/AoC-Day2-RedNosedReports/src/main/java/org/aoc2024/Main.java
+++ AoC2024/AoC-Day2-RedNosedReports/src/main/java/org/aoc2024/Main.java
@@ -2,6 +2,10 @@ package org.aoc2024;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final Solution solution = new Solution();
+//        solution.getSolution("puzzle.txt", 1);
+//        System.out.println("PART_1 safe levels: " + solution.getSafeReportsCount());
+        solution.getSolution("puzzle.txt", 2);
+        System.out.println("PART_2 safe levels: " + solution.getSafeReportsCount());
     }
 }
