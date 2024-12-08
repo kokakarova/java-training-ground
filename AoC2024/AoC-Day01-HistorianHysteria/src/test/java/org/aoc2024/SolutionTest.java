3fc0c585b825c2ce1d990fa364825d0f7e7cb269
kokakarova <kostadinkakarova@gmail.com>
2024-12-08 14:14:58 +0100

    test: add test for part 2

--- AoC2024/AoC-Day01-HistorianHysteria/src/test/java/org/aoc2024/SolutionTest.java
+++ AoC2024/AoC-Day01-HistorianHysteria/src/test/java/org/aoc2024/SolutionTest.java
@@ -1,9 +1,6 @@
 package org.aoc2024;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
@@ -11,8 +8,16 @@ class SolutionTest {
     private final Solution solution = new Solution();
 
     @Test
-    void shouldPrintColumns() {
+    void shouldReturn11_ForPuzzlePart1() {
         String fileName = "puzzleTest.txt";
-        assertEquals(11, solution.readFromFile(fileName));
+        assertEquals(11, solution.getSolution(fileName, 1));
     }
+
+    @Test
+    void shouldReturn31_ForPuzzlePart2() {
+        String fileName = "puzzleTest.txt";
+        assertEquals(31, solution.getSolution(fileName, 2));
+    }
+
+
 }
