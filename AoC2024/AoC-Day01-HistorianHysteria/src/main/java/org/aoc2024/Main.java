b19e1405196202e96b0ed0e7113c05dd78248a9b
kokakarova <kostadinkakarova@gmail.com>
2024-12-08 14:15:15 +0100

    feat: solve for part 2

--- AoC2024/AoC-Day01-HistorianHysteria/src/main/java/org/aoc2024/Main.java
+++ AoC2024/AoC-Day01-HistorianHysteria/src/main/java/org/aoc2024/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         final Solution solution = new Solution();
         String fileName = "puzzle.txt";
-        System.out.println("Solution: " + solution.readFromFile(fileName));
+        System.out.println("Solution for Part 1: " + solution.getSolution(fileName, 1));
+        System.out.println("Solution for Part 2: " + solution.getSolution(fileName, 2));
     }
 }
--- AoC2024/AoC-Day01-HistorianHysteria/src/main/java/org/aoc2024/Solution.java
+++ AoC2024/AoC-Day01-HistorianHysteria/src/main/java/org/aoc2024/Solution.java
@@ -15,36 +15,58 @@ public class Solution {
     private List<Integer> leftColumn = new ArrayList<>();
     private List<Integer> rightColumn = new ArrayList<>();
 
-    public int readFromFile(String fileName) {
-//        int lineNumber = 0;
+    public int getSolution(String fileName, int partNumber) {
+        sortPuzzleInput(fileName);
+        if (partNumber == 1) {
+            return calculateDistances();
+        } else if (partNumber == 2) {
+            return getSimilarityScore();
+        } else {
+            throw new IllegalArgumentException("Invalid part number: " + partNumber + ". Part number can only be 1 or 2");
+        }
+    }
+
+    public void sortPuzzleInput(String fileName) {
         try (InputStream file = Main.class.getClassLoader().getResourceAsStream(fileName)) {
             List<Integer> leftUnsorted = new ArrayList<>();
             List<Integer> rightUnsorted = new ArrayList<>();
-            if (file == null) {
-                System.out.println("file is null");
-            } else {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(file));
-                for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-                    String[] bothColumns = line.split(" ");
-                    leftUnsorted.add(Integer.parseInt(bothColumns[0]));
-                    rightUnsorted.add(Integer.parseInt(bothColumns[bothColumns.length - 1]));
-                }
+            assert file != null;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(file));
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                String[] bothColumns = line.split(" ");
+                leftUnsorted.add(Integer.parseInt(bothColumns[0]));
+                rightUnsorted.add(Integer.parseInt(bothColumns[bothColumns.length - 1]));
+            }
                 leftColumn = leftUnsorted.stream().sorted().toList();
                 rightColumn = rightUnsorted.stream().sorted().toList();
-
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return calculateDistances();
     }
 
     private int calculateDistances() {
-       int sum = 0;
-       for (int i = 0; i < leftColumn.size(); i++) {
-           sum += Math.abs(leftColumn.get(i) - rightColumn.get(i));
-       }
-       return sum;
+        int sum = 0;
+        for (int i = 0; i < leftColumn.size(); i++) {
+            sum += Math.abs(leftColumn.get(i) - rightColumn.get(i));
+        }
+        return sum;
     }
 
+    private int getSimilarityScore() {
+        int sum = 0;
+        int prevNumber = -1;
+        int prevNumberMultiplied = -1;
+        List<Integer> rightColumnMutable = new ArrayList<>(rightColumn);
+        for (Integer integer : leftColumn) {
+            if (integer != prevNumber) {
+                prevNumber = integer;
+                int startRightColumnSize = rightColumnMutable.size();
+                List<Integer> numbersToRemove = List.of(integer);
+                rightColumnMutable.removeAll(numbersToRemove);
+                prevNumberMultiplied = integer * (startRightColumnSize - rightColumnMutable.size());
+            }
+            sum += prevNumberMultiplied;
+        }
+        return sum;
+    }
 }
