4dbcb4e7902016a7ef32e27cfe667709c0e3b92e
kokakarova <kostadinkakarova@gmail.com>
2024-12-10 00:27:38 +0100

    test: add test for part 2

--- AoC2024/AoC-Day03-MullItOver/src/test/java/org/aoc2024/SolutionTest.java
+++ AoC2024/AoC-Day03-MullItOver/src/test/java/org/aoc2024/SolutionTest.java
@@ -8,11 +8,18 @@ class SolutionTest {
     Solution solution = new Solution();
 
     @Test
-    void shouldGet161_forPuzzleTest() {
-        String fileName = "puzzleTest.txt";
-        solution.getSolution(fileName);
+    void shouldGet161_forPuzzleTest_Part1() {
+        String fileName = "puzzleTest1.txt";
+        solution.getSolution(fileName, 1);
         int actualResult = solution.getSumOfMultiples();
         assertEquals(161, actualResult);
+    }
+    @Test
+    void shouldGet48_forPuzzleTest_Part2() {
+        String fileName = "puzzleTest2.txt";
+        solution.getSolution(fileName, 2);
+        int actualResult = solution.getSumOfMultiples();
+        assertEquals(48, actualResult);
     }
 
 }
