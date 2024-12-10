cb18336212d9c3637d4cf41be53cd60c2ce6e06f
kokakarova <kostadinkakarova@gmail.com>
2024-12-10 13:49:44 +0100

    feat: solve for part 1

--- AoC2024/AoC-Day04-CeresSearch/src/main/java/org/aoc2024/Main.java
+++ AoC2024/AoC-Day04-CeresSearch/src/main/java/org/aoc2024/Main.java
@@ -2,6 +2,13 @@ package org.aoc2024;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Solution solution = new Solution();
+        int rows = 140;
+        int cols = 140;
+        String fileName = "puzzle.txt";
+        String word ="XMAS";
+        char[][] puzzleGrid = solution.getPuzzleGridFromInput(fileName, rows, cols);
+        solution.searchWord(puzzleGrid,word);
+        System.out.println("PART_1 solution: " + Solution.matchedWordsCount);
     }
 }
--- AoC2024/AoC-Day04-CeresSearch/src/main/java/org/aoc2024/Solution.java
+++ AoC2024/AoC-Day04-CeresSearch/src/main/java/org/aoc2024/Solution.java
@@ -1,12 +1,15 @@
 package org.aoc2024;
 
+import lombok.Data;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+@Data
 public class Solution {
-    private static int matchedWordsCount = 0;
+    public static int matchedWordsCount = 0;
 
     public char[][] getPuzzleGridFromInput(String fileName, int rows, int cols) {
         char[][] puzzle = new char[rows][cols];
@@ -24,5 +27,48 @@ public class Solution {
             e.printStackTrace();
         }
         return puzzle;
+    }
+
+    private int search2DGrid(char[][] puzzleGrid, int row, int col, String word) {
+        int m = puzzleGrid.length;
+        int n = puzzleGrid[0].length;
+        int wordsFound = 0;
+        if (puzzleGrid[row][col] != word.charAt(0)) {
+            return 0;
+        }
+        int len = word.length();
+        int[] x = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] y = {-1, 0, 1, -1, 1, -1, 0, 1};
+        int directions = 8;
+        for (int dir = 0; dir < directions; dir++) {
+            int k;
+            int currX = row + x[dir];
+            int currY = col + y[dir];
+            for (k = 1; k < len; k++) {
+                if (currX >= m || currX < 0 || currY >= n || currY < 0) {
+                    break;
+                }
+                if (puzzleGrid[currX][currY] != word.charAt(k)) {
+                    break;
+                }
+                currX += x[dir];
+                currY += y[dir];
+            }
+            if (k == len) {
+                wordsFound++;
+            }
+        }
+        return wordsFound;
+    }
+
+    public void searchWord(char[][] puzzleGrid, String word) {
+        int m = puzzleGrid.length;
+        int n = puzzleGrid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matchedWordsCount += search2DGrid(puzzleGrid, i, j, word);
+            }
+        }
+
     }
 }
