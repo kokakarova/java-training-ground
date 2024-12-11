2d32aafacf453bf65930a746f33b1b9ddd888cce
kokakarova <kostadinkakarova@gmail.com>
2024-12-11 22:26:15 +0100

    test: assert correctness

--- AoC2024/AoC-Day05-PrintQueue/src/test/java/org/aoc2024/SolutionTest.java
+++ AoC2024/AoC-Day05-PrintQueue/src/test/java/org/aoc2024/SolutionTest.java
@@ -10,10 +10,11 @@ class SolutionTest {
 
     @Test
     void shouldReturn143_forPuzzleTest_Part1() {
-        int exprectedResult = 143;
+        Integer expectedResult = 143;
         String fileName = "puzzleTest.txt";
         solution.getSolution(fileName);
-        System.out.println(solution.getRulesMap().toString());
+        Integer actualResult = solution.getSumOfMiddles();
+        assertEquals(expectedResult, actualResult);
     }
 
 }
