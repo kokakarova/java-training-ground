80bf5154fece41d108917ca58c4ac73ded1e6d1e
kokakarova <kostadinkakarova@gmail.com>
2024-12-11 23:50:31 +0100

    add test file and puzzle input

---
+++ AoC2024/AoC-Day06-GuardGallivant/src/test/java/org/aoc2024/SolutionTest.java
@@ -0,0 +1,15 @@
+package org.aoc2024;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    Solution solution = new Solution();
+
+    @Test
+    void shouldReturn41_ForPuzzleTest_Part1() {
+
+    }
+
+}
