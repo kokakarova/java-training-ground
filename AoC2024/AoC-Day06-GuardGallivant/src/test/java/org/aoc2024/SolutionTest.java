99e8baa65b5e6ea8f3adb9e21ae8421be4bcd4a4
kokakarova <kostadinkakarova@gmail.com>
2024-12-12 21:37:14 +0100

    test: pass test

--- AoC2024/AoC-Day06-GuardGallivant/src/test/java/org/aoc2024/SolutionTest.java
+++ AoC2024/AoC-Day06-GuardGallivant/src/test/java/org/aoc2024/SolutionTest.java
@@ -9,7 +9,20 @@ class SolutionTest {
 
     @Test
     void shouldReturn41_ForPuzzleTest_Part1() {
-
+        int expectedResult = 41;
+        String fileName = "puzzleTest.txt";
+        int rows = 10;
+        int cols = 10;
+        char[][] grid = solution.getPatrolGrid(fileName, rows, cols);
+        solution.getSolution(grid);
+        assertEquals(expectedResult, solution.stepsCount);
     }
+    @Test
+    void shouldReturn1_ForInputMovement0() {
+        int expectedResult = 1;
+        int movement = 0;
+        assertEquals(expectedResult, solution.updateMovementDirection(movement));
+    }
+
 
 }
