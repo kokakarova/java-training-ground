f0a5df542f00e606608d0c70eabb72cd7818bf05
kokakarova <kostadinkakarova@gmail.com>
2024-12-12 21:36:59 +0100

    feat: solve for part 1

--- AoC2024/AoC-Day06-GuardGallivant/src/main/java/org/aoc2024/Main.java
+++ AoC2024/AoC-Day06-GuardGallivant/src/main/java/org/aoc2024/Main.java
@@ -1,7 +1,14 @@
 package org.aoc2024;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Solution solution = new Solution();
+        int rows = 130;
+        int cols = 130;
+        String fileName = "puzzle.txt";
+        char[][] grid = solution.getPatrolGrid(fileName, rows, cols);
+        solution.getSolution(grid);
+        System.out.println("PART_1 solution ----> " + solution.stepsCount);
     }
 }
--- AoC2024/AoC-Day06-GuardGallivant/src/main/java/org/aoc2024/Solution.java
+++ AoC2024/AoC-Day06-GuardGallivant/src/main/java/org/aoc2024/Solution.java
@@ -6,11 +6,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class Solution {
 
     int[] startPosition = new int[2];
+    int stepsCount = 1;
 
     public char[][] getPatrolGrid(String fileName, int rows, int cols) {
         char[][] grid = new char[rows][cols];
@@ -36,6 +39,50 @@ public class Solution {
             e.printStackTrace();
         }
         return grid;
+    }
+
+    public void getSolution(char[][] grid) {
+        List<String> visitedPositions = new ArrayList<>();
+        int movement = 0;
+        int len = grid.length;
+        boolean inScope = true;
+        int[] currentPosition = startPosition;
+        int[] nextStepCoords = getNextStep(currentPosition, movement);
+        String visited = currentPosition[0] + "," + currentPosition[1];
+        visitedPositions.add(visited);
+        while (inScope) {
+            if (!checkStepScope(nextStepCoords, len)) {
+                inScope = false;
+            } else {
+                char charAtNextStep = grid[nextStepCoords[0]][nextStepCoords[1]];
+                if (charAtNextStep == '.') {
+                    currentPosition = nextStepCoords;
+                    nextStepCoords = getNextStep(currentPosition, movement);
+                    if (!visitedPositions.contains(currentPosition[0] + "," + currentPosition[1])) {
+                        stepsCount++;
+                        visitedPositions.add(currentPosition[0] + "," + currentPosition[1]);
+                    }
+                } else {
+                    movement = updateMovementDirection(movement);
+                    nextStepCoords = getNextStep(currentPosition, movement);
+                }
+            }
+        }
+    }
+
+    public int updateMovementDirection(int movement) {
+        return movement == 3 ? 0 : movement + 1;
+    }
+
+    private int[] getNextStep(int[] currentPosition, int movement) {
+        int[][] movingDirections = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        return new int[]{currentPosition[0] + movingDirections[movement][0], currentPosition[1] + movingDirections[movement][1]};
+    }
+
+    private boolean checkStepScope(int[] nextStep, int len) {
+        int xCoord = nextStep[0];
+        int yCoord = nextStep[1];
+        return xCoord >= 0 && xCoord < len && yCoord >= 0 && yCoord < len;
     }
 }
 
