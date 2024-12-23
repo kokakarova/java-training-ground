b16650285e4adfad585dcd5906f4d18891548e22
kokakarova <kostadinkakarova@gmail.com>
2024-12-23 16:05:56 +0100

    chore: call methods for part 2

--- AoC2024/AoC-Day06-GuardGallivant/src/main/java/org/aoc2024/Main.java
+++ AoC2024/AoC-Day06-GuardGallivant/src/main/java/org/aoc2024/Main.java
@@ -5,13 +5,16 @@ public class Main {
     public static void main(String[] args) {
         GuardSteps guardSteps = new GuardSteps();
         FileReader fileReader = new FileReader();
+        Obstacles obstacles = new Obstacles();
         int rows = 130;
         int cols = 130;
         String fileName = "puzzle.txt";
         StartingGrid grid = fileReader.getPatrolGrid(fileName, rows, cols);
-        guardSteps.countGuardSteps(grid, 0);
-        System.out.println("PART_1 solution ----> " + guardSteps.stepsCount);
-//        solutionPart1.getSolution(grid, 2, 0);
-//        System.out.println("PART_2 solution ----> " + solutionPart1.stepsCount);
+//        guardSteps.countGuardSteps(grid, 0);
+//        System.out.println("PART_1 solution ----> " + guardSteps.stepsCount);
+
+        Navigator nav = new Navigator(0, grid.getStartingPosition(), grid.getNextToStartingPosition());
+        obstacles.countObstacles(grid,nav, false);
+        System.out.println("PART_2 solution ----> " + obstacles.getObstacleCount());
     }
 }
