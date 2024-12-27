44e87e9a561a231a6265610aceb3da5c10218efd
kokakarova <kostadinkakarova@gmail.com>
2024-12-27 13:11:53 +0100

    feat: revert to recursive method for binary iteration

--- AoC2024/AoC-Day07-BridgeRepair/src/main/java/org/aoc2024/InputProcessor.java
+++ AoC2024/AoC-Day07-BridgeRepair/src/main/java/org/aoc2024/InputProcessor.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import org.apache.commons.lang3.StringUtils;
 
 @Data
 public class InputProcessor {
@@ -31,8 +30,7 @@ public class InputProcessor {
         Equation equation = splitInputString(line);
         int operatorPlaces = equation.getEqNumbers().length - 1;
         int[] eqOperators = new int[operatorPlaces];
-//        if (checkForAllOperatorsPermutationsRecursive(0, eqOperators, operatorPlaces, equation)) {
-        if (checkForAllOperatorsNumToBin(operatorPlaces, equation)) {
+        if (checkForAllOperatorsPermutationsRecursive(0, eqOperators, operatorPlaces, equation)) {
             Equation.validEquations += equation.getEqResult();
         }
     }
@@ -53,39 +51,11 @@ public class InputProcessor {
         if (i == opPlaces) {
             return equation.isValidEquation(equation, arr);
         }
-        arr[i] = 0; // 0, 0, 0
+        arr[i] = 0;
         if (checkForAllOperatorsPermutationsRecursive(i + 1, arr, opPlaces, equation)) {
             return true;
         }
-        arr[i] = 1; // 1, 0, 0
+        arr[i] = 1;
         return checkForAllOperatorsPermutationsRecursive(i + 1, arr, opPlaces, equation);
     }
-
-    public boolean checkForAllOperatorsNumToBin(int opPlaces, Equation equation) {
-        String highestBinaryString = StringUtils.leftPad("", opPlaces, "1");
-        int highestNum = Integer.parseInt(highestBinaryString, 2);
-        int[] eqOperators = new int[opPlaces];
-        for (int i = 0; i <= highestNum; i++) {
-            // convert i to binary string
-            String binaryI = StringUtils.leftPad(Integer.toBinaryString(i), opPlaces, "0");
-            // populate operators arrays as per binary string
-            populateArrayWithBinaryChars(eqOperators, binaryI);
-            // check with that binary string
-            if (equation.isValidEquation(equation, eqOperators)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void populateArrayWithBinaryChars(int[] eqOperators, String binaryString) {
-        int len = binaryString.length();
-        for (int i = 0; i < len; i++) {
-            eqOperators[i] = Integer.parseInt(String.valueOf(binaryString.charAt(i)));
-        }
-    }
-
-//    private int getHighestNumForBinaryConversion(int opPlaces) {
-//
-//    }
 }
