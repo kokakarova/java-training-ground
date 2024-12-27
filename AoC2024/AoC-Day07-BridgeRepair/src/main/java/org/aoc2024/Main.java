398b4d98246379674811399aed78ad140418d163
kokakarova <kostadinkakarova@gmail.com>
2024-12-27 13:09:45 +0100

    feat: add code to show execution time

--- AoC2024/AoC-Day07-BridgeRepair/src/main/java/org/aoc2024/Main.java
+++ AoC2024/AoC-Day07-BridgeRepair/src/main/java/org/aoc2024/Main.java
@@ -1,11 +1,18 @@
 package org.aoc2024;
 
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class Main {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         String fileName = "puzzle.txt";
         InputProcessor ip = new InputProcessor();
         ip.readEquationsFromInput(fileName);
         System.out.println("Part 1 RESULT ---> " + Equation.validEquations);
+        long endTime   = System.currentTimeMillis();
+        NumberFormat formatter = new DecimalFormat("#0.00000");
+        System.out.print("Execution time is " + formatter.format((endTime - startTime) / 1000d) + " seconds");
     }
 }
