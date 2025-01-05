c8bc45af30989412e7a2b4584c4d7d0ab318d53f
kokakarova <kostadinkakarova@gmail.com>
2025-01-05 14:00:42 +0100

    test: add more tests

---
+++ AoC2024/Aoc-Day09-DiskFragmenter/src/main/java/org/aoc2024/PuzzleProcessor.java
@@ -0,0 +1,141 @@
+package org.aoc2024;
+
+import lombok.Data;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class PuzzleProcessor {
+
+    public List<Integer> readFromInputFile(String fileName) {
+        List<Integer> disk = new ArrayList<>();
+        try (InputStream file = Main.class.getClassLoader().getResourceAsStream(fileName)) {
+            assert file != null;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(file));
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                for (int i = 0, j = 0; i < line.length(); i++, j++) {
+                    int charAsInt = Character.getNumericValue(line.charAt(i));
+                    // add charAsInt to places as id
+                    disk.addAll(addFileToString(j, charAsInt));
+                    i++;
+                    // add -1 for empty space
+                    if (i < line.length() - 1) {
+                        int nextCharAsInt = Character.getNumericValue(line.charAt(i));
+                        if (nextCharAsInt > 0) {
+                            disk.addAll(addFileToString(-1, nextCharAsInt));
+                        }
+                    }
+                }
+            }
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        return disk;
+    }
+
+    public List<Integer> addFileToString(int numberToAdd, int timesToRepeat) {
+        List<Integer> newList = new ArrayList<>();
+        for (int i = 0; i < timesToRepeat; i++) {
+            newList.add(numberToAdd);
+        }
+        return newList;
+    }
+
+    public List<Integer> processFileCompactingPart1(List<Integer> diskList) {
+        int endFileIndex = getIndexOfOneFilePartAtEnd(diskList);
+        final int SPACE_MARK = -1;
+        int indexOfFirstSpace = diskList.indexOf(SPACE_MARK);
+        while (indexOfFirstSpace < endFileIndex) {
+            diskList.set(indexOfFirstSpace, diskList.get(endFileIndex));
+            diskList.set(endFileIndex, SPACE_MARK);
+            endFileIndex = getIndexOfOneFilePartAtEnd(diskList);
+            indexOfFirstSpace = diskList.indexOf(SPACE_MARK);
+        }
+        return diskList;
+    }
+
+    private int getIndexOfOneFilePartAtEnd(List<Integer> diskList) {
+        for (int i = diskList.size() - 1; i >= 0; i--) {
+            if (diskList.get(i) != -1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public long getFilesystemCheckSum(List<Integer> diskList) {
+        long checkSum = 0;
+        for (int i = 0; i < diskList.size(); i++) {
+            if (diskList.get(i) != -1) {
+                checkSum += diskList.get(i) * i;
+            }
+        }
+        return checkSum;
+    }
+
+    public List<Integer> processFileCompactingPart2(List<Integer> diskList) {
+        int rightMostFileId = getIndexOfOneFilePartAtEnd(diskList);
+        while (rightMostFileId >= 0) {
+//             diskList = exploreCompactingForFileId(rightMostFileId);
+            // get right-most file chunk (indexes)
+            List<Integer> wholeFileIndexes = getIndexesForFileId(diskList, rightMostFileId);
+            // find fitting free space chunk from left to right (indexes)
+            List<Integer> freeSpaceChunks = getAvailableFreeSpaceChunk(diskList, wholeFileIndexes.size());
+            // rearrange file chunk and free space chunk
+            if (!freeSpaceChunks.isEmpty() && freeSpaceChunks.getFirst() < wholeFileIndexes.getFirst()) {
+                diskList = replaceSpaceWithFile(diskList, rightMostFileId, freeSpaceChunks);
+                diskList = replaceFileIndexesWithSpace(diskList, wholeFileIndexes);
+            }
+            rightMostFileId--;
+        }
+        return diskList;
+    }
+
+    public List<Integer> replaceFileIndexesWithSpace(List<Integer> diskList, List<Integer> wholeFileIndexes) {
+        for (Integer wholeFileIndex : wholeFileIndexes) {
+            diskList.set(wholeFileIndex, -1);
+        }
+        return diskList;
+    }
+
+    public List<Integer> replaceSpaceWithFile(List<Integer> diskList, int rightMostFileId, List<Integer> freeSpaceChunks) {
+        for (Integer freeSpaceChunk : freeSpaceChunks) {
+            diskList.set(freeSpaceChunk, rightMostFileId);
+        }
+        return diskList;
+    }
+
+    public List<Integer> getAvailableFreeSpaceChunk(List<Integer> diskList, int size) {
+        int index = diskList.indexOf(-1);
+        List<Integer> freeSpaceChunks = new ArrayList<>();
+        while (freeSpaceChunks.size() < size && index != -1) {
+            freeSpaceChunks.add(index);
+            if (diskList.get(index + 1) == -1) {
+                index++;
+            } else {
+                index = -1;
+            }
+        }
+        return freeSpaceChunks.size() == size ? freeSpaceChunks : new ArrayList<>();
+    }
+
+    public List<Integer> getIndexesForFileId(List<Integer> diskList, int fileId) {
+        int index = diskList.indexOf(fileId);
+        List<Integer> fileIndexes = new ArrayList<>();
+        while (index != -1) {
+            fileIndexes.add(index);
+            if (index + 1 < diskList.size() && diskList.get(index + 1) == fileId) {
+                index++;
+            } else {
+                index = -1;
+            }
+        }
+        return fileIndexes;
+    }
+}
---
+++ AoC2024/Aoc-Day09-DiskFragmenter/src/test/java/org/aoc2024/PuzzleProcessorTest.java
@@ -0,0 +1,173 @@
+package org.aoc2024;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PuzzleProcessorTest {
+
+    @Test
+    void shouldReturn_111String_ForInput_int3_Char1() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+        List<Integer> expected = List.of(1, 1, 1);
+        assertEquals(expected, processor.addFileToString(1, 3));
+    }
+
+    @Test
+    void shouldReturn_ExpectedForPuzzleTest_Part1() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+//        String expected = "00...111...2...333.44.5555.6666.777.888899";
+        List<Integer> expected = List.of(0, 0, -1, -1, -1, 1, 1, 1, -1, -1, -1, 2, -1, -1, -1, 3, 3, 3, -1, 4, 4, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, 7, 7, 7, -1, 8, 8, 8, 8, 9, 9);
+        String fileName = "puzzleTest.txt";
+        assertEquals(expected, processor.readFromInputFile(fileName));
+    }
+
+    @Test
+    void shouldReturn_ExpectedForPuzzleTest2_Part1() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+//        String expected = "0..111....22222";
+        List<Integer> expected = List.of(0, -1, -1, 1, 1, 1, -1, -1, -1, -1, 2, 2, 2, 2, 2);
+        String fileName = "puzzleTest2.txt";
+        assertEquals(expected, processor.readFromInputFile(fileName));
+    }
+
+    @Test
+    void shouldReturn_ExpectedArrangedDiskList_ForPuzzleTest2_Part1() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+//        String expected = "022111222......";
+        List<Integer> expected = List.of(0, 2, 2, 1, 1, 1, 2, 2, 2, -1, -1, -1, -1, -1, -1);
+        String fileName = "puzzleTest2.txt";
+        List<Integer> initialProcessedDiskList = processor.readFromInputFile(fileName);
+        assertEquals(expected, processor.processFileCompactingPart1(initialProcessedDiskList));
+    }
+
+    @Test
+    void shouldReturn_ExpectedArrangedDiskList_ForPuzzleTest_Part1() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+//        String expected = "0099811188827773336446555566..............";
+        List<Integer> expected = List.of(0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4, 4, 6, 5, 5, 5, 5, 6, 6, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+        String fileName = "puzzleTest.txt";
+        List<Integer> initialProcessedDiskList = processor.readFromInputFile(fileName);
+        assertEquals(expected, processor.processFileCompactingPart1(initialProcessedDiskList));
+    }
+
+    @Test
+    void shouldReturn1928_ForPuzzleTest_Part1() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+        int expected = 1928;
+        String fileName = "puzzleTest.txt";
+        List<Integer> initialProcessedDiskList = processor.readFromInputFile(fileName);
+        List<Integer> processedDiskList = processor.processFileCompactingPart1(initialProcessedDiskList);
+        assertEquals(expected, processor.getFilesystemCheckSum(processedDiskList));
+    }
+
+    @Test
+    void shouldReturn_ListOf3_ForInputList_01110() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+        int fileId = 1;
+        List<Integer> distTest = List.of(0, 1, 1, 1, 0);
+        List<Integer> expectedList = List.of(1, 2, 3);
+        List<Integer> actualResult = processor.getIndexesForFileId(distTest, fileId);
+        assertEquals(expectedList.size(), actualResult.size());
+        assertEquals(expectedList.get(0), actualResult.get(0));
+        assertEquals(expectedList.get(1), actualResult.get(1));
+        assertEquals(expectedList.get(2), actualResult.get(2));
+    }
+
+    @Test
+    void shouldReturn_ListOf3_ForInputList_0neg1neg1neg10() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+        List<Integer> distTest = List.of(0, -1, -1, -1, 0);
+        List<Integer> expectedList = List.of(1, 2, 3);
+        List<Integer> actualResult = processor.getAvailableFreeSpaceChunk(distTest, 3);
+        assertEquals(expectedList.size(), actualResult.size());
+        assertEquals(expectedList.get(0), actualResult.get(0));
+        assertEquals(expectedList.get(1), actualResult.get(1));
+        assertEquals(expectedList.get(2), actualResult.get(2));
+    }
+
+    @Test
+    void shouldReturn_ListOf0_ForInputList_01110() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+        List<Integer> distTest = List.of(0, 1, 1, 1, 0);
+        List<Integer> actualResult = processor.getAvailableFreeSpaceChunk(distTest, 3);
+        assertEquals(0, actualResult.size());
+    }
+
+    @Test
+    void shouldReturn_ListOf2_ForInputList_neg1neg1neg110_AndSize2() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+        List<Integer> distTest = List.of(-1, -1, -1, 1, 0);
+        List<Integer> actualResult = processor.getAvailableFreeSpaceChunk(distTest, 2);
+        assertEquals(2, actualResult.size());
+    }
+
+    @Test
+    void shouldReturn_ListOf2_ForInputList_0neg1neg1neg110_AndSize2() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+        List<Integer> distTest = List.of(0, -1, -1, -1, 1, 1);
+        List<Integer> actualResult = processor.getAvailableFreeSpaceChunk(distTest, 2);
+        assertEquals(2, actualResult.size());
+    }
+
+    @Test
+    void shouldReturn_ExpectedList_ForPuzzleTest_Part2() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+        String fileName = "puzzleTest.txt";
+//        String expectedString = "00992111777.44.333....5555.6666.....8888..";
+        List<Integer> expected = List.of(0, 0, 9, 9, 2, 1, 1, 1, 7, 7, 7, -1, 4, 4, -1, 3, 3, 3, -1, -1, -1, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, -1, -1, -1, -1, 8, 8, 8, 8, -1, -1);
+        List<Integer> initialProcessedDiskList = processor.readFromInputFile(fileName);
+        System.out.println("initialProcessedDiskList:");
+        for (Integer i : initialProcessedDiskList) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        List<Integer> processedDiskList = processor.processFileCompactingPart2(initialProcessedDiskList);
+        System.out.println("processedDiskList");
+        for (Integer i : processedDiskList) {
+            System.out.print(i + " ");
+        }
+        assertEquals(expected.size(), processedDiskList.size());
+    }
+
+    @Test
+    void shouldReturn2858_ForPuzzleTest_Part2() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+        String fileName = "puzzleTest.txt";
+        int expected = 2858;
+        List<Integer> initialProcessedDiskList = processor.readFromInputFile(fileName);
+        List<Integer> processedDiskList = processor.processFileCompactingPart2(initialProcessedDiskList);
+        assertEquals(expected, processor.getFilesystemCheckSum(processedDiskList));
+    }
+
+    // test for processFileCompactingPart2
+
+    // replaceSpaceWithFile
+    @Test
+    void shouldTest_replaceSpaceWithFile() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+        List<Integer> diskTest = new ArrayList<>(List.of(0, -1, -1, -1, 0));
+        int rightMostFieldTest = 10;
+        List<Integer> freeSpaceChunksTest = List.of(1, 2, 3);
+        List<Integer> expectedList = List.of(0, 10, 10, 10, 0);
+        var actualResult = processor.replaceSpaceWithFile(diskTest, rightMostFieldTest, freeSpaceChunksTest);
+        assertEquals(expectedList.size(), actualResult.size());
+    }
+
+    // replaceSpaceWithFile
+    @Test
+    void shouldTest_replaceFileIndexesWithSpace() {
+        PuzzleProcessor processor = new PuzzleProcessor();
+        List<Integer> diskTest = new ArrayList<>(List.of(0, 1, 1, 1, 0));
+        List<Integer> freeSpaceChunksTest = List.of(1, 2, 3);
+        List<Integer> expectedList = List.of(0, -1, -1, -1, 0);
+        var actualResult = processor.replaceFileIndexesWithSpace(diskTest, freeSpaceChunksTest);
+        assertEquals(expectedList.size(), actualResult.size());
+        assertEquals(expectedList, actualResult);
+    }
+
+}
