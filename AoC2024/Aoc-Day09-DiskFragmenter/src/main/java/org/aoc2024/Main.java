144352e4b798b3f8aec08cb536a3532be59e9b8a
kokakarova <kostadinkakarova@gmail.com>
2025-01-05 14:00:32 +0100

    feat: add code to measure execution time

--- AoC2024/Aoc-Day09-DiskFragmenter/src/main/java/org/aoc2024/Main.java
+++ AoC2024/Aoc-Day09-DiskFragmenter/src/main/java/org/aoc2024/Main.java
@@ -1,13 +1,21 @@
 package org.aoc2024;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        PuzzleProccessor proccessor = new PuzzleProccessor();
+        long startTime = System.currentTimeMillis();
+        PuzzleProcessor proccessor = new PuzzleProcessor();
         String fileName = "puzzle.txt";
         List<Integer> processedDiskList = proccessor.readFromInputFile(fileName);
-        List<Integer> formatedDiskList = proccessor.processFileCompacting(processedDiskList);
+        // PART 1
+        List<Integer> formatedDiskList = proccessor.processFileCompactingPart1(processedDiskList);
         System.out.println("Part 1 result ---> " + proccessor.getFilesystemCheckSum(formatedDiskList));
+        // PART 2
+        long endTime   = System.currentTimeMillis();
+        NumberFormat formatter = new DecimalFormat("#0.00000");
+        System.out.print("Execution time is " + formatter.format((endTime - startTime) / 1000d) + " seconds");
     }
 }
