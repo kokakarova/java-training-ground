117a0f7635aaf19384663c9c3ad2e9eb17c0b6ee
kokakarova <kostadinkakarova@gmail.com>
2025-01-14 21:16:55 +0100

    feat: call processFile method from main

--- AoC2024/AoC-Day10-HoofIt/src/main/java/org/aoc2024/Main.java
+++ AoC2024/AoC-Day10-HoofIt/src/main/java/org/aoc2024/Main.java
@@ -1,7 +1,13 @@
 package org.aoc2024;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PuzzleProcessor puzzleProcessor = new PuzzleProcessor();
+        String fileName = "puzzle.txt";
+        int dimensions = 54;
+        int[][] map = puzzleProcessor.readFromInputFile(fileName, dimensions, dimensions);
+        System.out.println("Arrays.deepToString(map) = " + Arrays.deepToString(map));
     }
 }
